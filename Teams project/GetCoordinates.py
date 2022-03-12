aa3d26585810bec2420a2a7d2a08ca6056ac9866
sanchit-cmd <94012874+sanchit-cmd@users.noreply.github.com>
2022-03-12 17:58:48 +0530

    1

---
+++ Teams project/GetCoordinates.py
@@ -0,0 +1,6 @@
+from pyautogui import *
+import time
+
+time.sleep(5)
+
+print(position())
---
+++ Teams project/app.py
@@ -0,0 +1,117 @@
+from pyautogui import *
+from time import sleep
+from datetime import *
+
+def open_teams(): #function to open the teams app
+    click(61 , 1064)#clicks the search button
+    sleep(1)
+    write("teams") #trypes "teams" in the search bar
+    sleep(1)
+    press("enter") #enter to open teams app
+    sleep(60)
+
+
+def close_teams(): #function toh close the teams app 
+    sleep(5)
+    click(1910, 13) #clicks on the X button to close the teams app
+
+def scroll():
+    sleep(5)
+    click(1910 , 280)#moves the cursor to the scroll bar and scrolls the page to the top of the screen
+    sleep(1)
+    dragTo(1910 , 543 , 3, button="left")#drags to the calender required
+
+
+
+def get_coordinates(): #function to return the corrditnate limit of the screen
+    return 1242,609
+
+
+def join_team(): #function to join a meeting
+    sleep(5)
+    click(38 , 304) #clicks on the caleder
+    sleep(3)
+    x,y = get_coordinates() #returns the coordinaate of the active class
+    doubleClick(x,y) # clicks on the class
+    sleep(8)
+    click(1761,101) #clicks on the join button_1
+    sleep(10)
+    click(1114 , 473) #clicks on the mute button
+    sleep(2)
+    click(1491 , 759) #clicks on the join button_2
+
+
+
+
+def leave_team(): #function to leave a meeting
+    sleep(10)
+    click(1614 , 52) #clicks the X button of the pop up window 
+    sleep(1)
+    click(1614 , 52) #blank click
+    sleep(2)
+    click(1849, 101) #clicks the close button
+
+
+
+open_teams() #opens the team app
+time_hours = datetime.now().hour
+while 9 <= time_hours <= 14: #loop will run while the time is between 9am and 2pm
+
+    time_hours = datetime.now().hour
+    time_min = datetime.now().minute
+    in_meeting  = False
+    
+
+    if time_hours == 9 and time_min >= 0:
+        if in_meeting == False:
+            join_team()
+            in_meeting = True
+        if time_hours == 9 and time_min >= 40:
+            if in_meeting == True:
+                leave_team()
+                in_meeting = False
+
+
+    elif time_hours == 10 and time_min >= 0:
+        if in_meeting == False:
+            join_team()
+            in_meeting = True
+        if time_hours == 10 and time_min >= 40:
+            if in_meeting == True:
+                leave_team()
+                in_meeting = False
+
+
+    elif time_hours == 11 and time_min >= 0:
+        if in_meeting == False:
+            join_team()
+            in_meeting = True
+        if time_hours == 11 and time_min >= 40:
+            if in_meeting == True:
+                leave_team()
+                in_meeting = False
+
+
+    elif time_hours == 12 and time_min >= 0:
+        if in_meeting == False:
+            join_team()
+            in_meeting = True
+        if time_hours == 12 and time_min >= 40:
+            if in_meeting == True:
+                leave_team()
+                in_meeting = False
+
+
+    elif time_hours == 13 and time_min >= 0:
+        if in_meeting == False:
+            join_team()
+            in_meeting = True
+        if time_hours == 13 and time_min >= 40:
+            if in_meeting == True:
+                leave_team()
+                in_meeting = False
+
+        
+
+
+close_teams() #closes the teams app
